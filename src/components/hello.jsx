b15a1b57f3a193eccac6dcc3f4a7858e12cead27
gosho-kazuya <gosho-kazuya@919.jp>
2017-08-13 20:58:39 +0900

    inquiry form

--- src/components/hello.jsx
+++ src/components/hello.jsx
@@ -1,9 +1,49 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-export default class Hello extends Component {
+export default class Hello extends React.Component {
+  constructor() {
+    super()
+    this.handleSend = this.handleSend.bind(this);
+
+    this.state = {
+      sum: 0,
+      text: '',
+      firstStep: true
+    }
+  }
+
+  handleSend() {
+    alert('Thanks')
+    this.setState({firstStep: true, text: ''})
+  }
+
   render() {
+    if (this.state.firstStep) {
+      return (
+        <div className="input">
+          <h1>入力 | お問い合わせ</h1>
+          <div>
+            <input type="text" value={this.state.text} onChange={event => this.setState({text: event.target.value})}></input>
+          </div>
+          <button className="button-primary" onClick={() => this.setState({firstStep: false})}>確認</button>
+        </div>
+      );
+    }
+
     return (
-      <h1>Hello</h1>
+      <div className="confirm">
+        <h1>確認 | お問い合わせ</h1>
+        Your inquiry:
+        <p>{this.state.text}</p>
+        <div className="row">
+          <div className="two columns">
+            <button onClick={() => this.setState({firstStep: true})}>Back</button>
+          </div>
+          <div className="two columns">
+            <button className="button-primary" onClick={this.handleSend}>送信</button>
+          </div>
+        </div>
+      </div>
     );
   }
 }
--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
-import { render } from 'react-dom';
+import ReactDOM from 'react-dom';
 
 import Hello from './components/hello.jsx';
-import World from './components/world.jsx';
 
-render(
+ReactDOM.render(
   <div>
     <Hello />
-    <World />
   </div>,
   document.getElementById('app')
 );
